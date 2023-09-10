46e731ad6ba6a5af2b59ca384700e51635d7d4dc
soraaaa-02 <ks14215ky14@gmail.com>
2023-09-10 21:18:14 +0900

    deviseに合わせて修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'login', to: 'user_sessions#new'
-  post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
-  resources :users, only: %i[new create]
+  devise_for :users
   root to: 'top#index'
 end
